df7a43924acb250c52b460bd40a15e8b34ccffdd
Chubbymaster <awthedragon@gmail.com>
2016-06-06 14:27:01 -0400

    pig player

---
+++ Pig.java
@@ -0,0 +1,12 @@
+package prokins;
+
+public class Pig {
+	private int health = 1920;
+	private Point location = new Point(1920/2,1080/2);
+	public void Pig(){
+		
+	}
+	public Point getLocation(){
+		return location;
+	}
+}

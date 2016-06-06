d1b44fc8517f3b7e59e4cf6119f8ad391ebc97ff
Chubbymaster <awthedragon@gmail.com>
2016-06-06 14:10:52 -0400

    finished hitbox and location generation
    pretty much done?

--- Aminal.java
+++ Aminal.java
@@ -6,13 +6,17 @@ public class Aminal{
 	private boolean right = false;
 	private String type;
 	private double speed;
-	
+	private Polygon hitbox;
+	private Point p;
 	
 	public void Animal(String animalType, boolean direction){
 		type = animalType;
 		right = direction;
 		Random randy = new Random();
 		speed = randy.nextDouble()*100;
+		Point[] boxP = {new Point(0,0), new Point(100,0), new Point(100,90), new Point(0,90)};
+		p = new Point(randy.nextInt(1720)+101,980);
+		hitbox = new Polygon(boxP,p,0);
 	}
 	
 	public void turn(){

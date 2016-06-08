1ee1366eef61622d68f08a024287d7c1d23ce962
Chubbymaster <awthedragon@gmail.com>
2016-06-08 13:41:23 -0400

    suddenly movemenet

--- Pig.java
+++ Pig.java
@@ -9,4 +9,46 @@ public class Pig {
 	public Point getLocation(){
 		return location;
 	}
+	
+	public void incrementHealth(){
+		health--;
+	}
+	
+	
+	public void eatBacon(){
+		if(health<1280){
+			health+=640;
+		}
+		else{
+			health = 1920;
+		}
+	}
+	
+	
+	public void fall(){
+		if(flight > 4){
+			flight = 4;
+			gravity = 1;
+		}
+		if(location.y >=200){
+			gravity = 0;
+		}
+		location = new Point(location.y+gravity,location.x);
+		gravity*=1.3;
+		
+	}
+	
+	
+	public void flap(){
+		gravity = 0;
+		location = new Point(location.y-flight, location.x);
+		flight*=1.03;
+		//fly up half the screen
+		//fly up for 1/2 a second
+	}
+	
+	
+	public void moveRight(){
+		location = new Point(location.y, location.x+1);
+	}
 }
